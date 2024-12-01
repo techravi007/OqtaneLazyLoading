5ba92181d2ecc3583a25fc95e59bcf1094dce817
Ravindra Kumar <ravindra.kumar@aitouch.in>
2024-12-01 15:04:23 +0530

    Modified proj reference to default and removed bin-obj

--- Package/debug.cmd
+++ Package/debug.cmd
@@ -1,7 +1,7 @@
-XCOPY "..\Client\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Client.Oqtane.dll" "..\..\oqtane520\Oqtane.Server\bin\Debug\net8.0\" /Y
-XCOPY "..\Client\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Client.Oqtane.pdb" "..\..\oqtane520\Oqtane.Server\bin\Debug\net8.0\" /Y
-XCOPY "..\Server\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Server.Oqtane.dll" "..\..\oqtane520\Oqtane.Server\bin\Debug\net8.0\" /Y
-XCOPY "..\Server\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Server.Oqtane.pdb" "..\..\oqtane520\Oqtane.Server\bin\Debug\net8.0\" /Y
-XCOPY "..\Shared\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Shared.Oqtane.dll" "..\..\oqtane520\Oqtane.Server\bin\Debug\net8.0\" /Y
-XCOPY "..\Shared\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Shared.Oqtane.pdb" "..\..\oqtane520\Oqtane.Server\bin\Debug\net8.0\" /Y
-XCOPY "..\Server\wwwroot\*" "..\..\oqtane520\Oqtane.Server\wwwroot\" /Y /S /I
+XCOPY "..\Client\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Client.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Client\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Client.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Server\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Server.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Server\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Server.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Shared\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Shared.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Shared\bin\Debug\net8.0\LLM.Module.LazyLoadingTest.Shared.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Server\wwwroot\*" "..\..\oqtane.framework\Oqtane.Server\wwwroot\" /Y /S /I
--- Package/release.cmd
+++ Package/release.cmd
@@ -1,4 +1,4 @@
 del "*.nupkg"
-"..\..\oqtane520\oqtane.package\nuget.exe" pack LLM.Module.LazyLoadingTest.nuspec 
-XCOPY "*.nupkg" "..\..\oqtane520\Oqtane.Server\Packages\" /Y
+"..\..\oqtane.framework\oqtane.package\nuget.exe" pack LLM.Module.LazyLoadingTest.nuspec 
+XCOPY "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\Packages\" /Y
 
